10a1be8f2e9c729c022df1e68b7cffa236092035
Vitiok2021 <viktorpts1vol@gmail.com>
2025-11-11 21:18:51 +0200

    ptacticed with light todo

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,9 +1,10 @@
+import { NgIf } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-child',
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './child.component.html',
   styleUrl: './child.component.scss',
 })
@@ -14,5 +15,29 @@ export class ChildComponent {
 
   sendToParrent() {
     this.mesToPar.emit(this.name);
+  }
+
+  isVisible: boolean = false;
+  todo!: string;
+  toDoList: any[] = [
+    {
+      title: 'Випити кави',
+    },
+    {
+      title: 'Випити чаю',
+    },
+    {
+      title: "З'їсти печиво",
+    },
+  ];
+  showed() {
+    this.isVisible = !this.isVisible;
+  }
+  addItem() {
+    if (!this.todo || this.todo.trim() === '') {
+      return;
+    }
+    this.toDoList.push({ title: this.todo });
+    this.todo = '';
   }
 }
